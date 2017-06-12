654b3df76c2c4f4a2e5af0f80cbd3f6a267dcab6
mattkohl <matt.kohl@thomsonreuters.com>
2017-06-12 10:02:08 +0100

    moved SPARK_HOME assignment to install-spark.sh

--- install-spark.sh
+++ install-spark.sh
@@ -5,3 +5,5 @@ HADOOP_VERSION='2.7'
 
 curl http://apache.mirror.gtcomm.net/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz --output /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
 cd /tmp && tar -xvzf /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
+
+export SPARK_HOME=/tmp/spark-2.1.1-bin-hadoop2.7
